fc21594485e8966d0f5f10d16aee457ce53052dd
Brian Adams <brianadamsmaino68@gmail.com>
2023-07-03 18:16:51 +0200

    Solved Lab

--- app.js
+++ app.js
@@ -1,63 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('public'));
+//makes publick available
+app.use(express.static("public"));
 
-app.get('/home', (request, response, next) => {
-  console.log(request);
-  response.send('<h1>First lab</h1>');
+//first route
+app.get("/home", (request, response, next) =>
+  response.sendFile(__dirname + "/views/home.html")
+);
+
+//about route
+app.get("/about", (request, response, next) =>
+  response.sendFile(__dirname + "/views/about.html")
+);
+
+//work route
+app.get("/works", (request, response, next) =>
+  response.sendFile(__dirname + "/views/works.html")
+);
+
+//gallery route
+app.get("/gallery", (request, response, next) =>
+  response.sendFile(__dirname + "/views/gallery.html")
+);
+
+//node app.js to run it once, or nodemon app.js that it keeps on running and updated automatically.
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
 });
-
-app.get('/about', (request, response) => {
-  response.send(`
-    <!doctype html>
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <title>About</title>
-        <link rel="stylesheet" href="/stylesheets/style.css" />
-      </head>
-      <body>
-      <h1>About Me!</h1>
-      <p>I'm Brian from Buenos Aires, Argentina</p>
-      <img src="/images/brian.JPG" alt="">
-      </body>
-    </html>
-  `);
-});
-
-app.get('/person', (request, response, next) => {
-  response.send(`
-      <!doctype html>
-      <html>
-        <head>
-          <meta charset="utf-8">
-          <title>Me</title>
-          <link rel="stylesheet" href="/stylesheets/style.css" />
-        </head>
-        <body>
-          <h1>Me</h1>
-          <p>This is my second route</p>
-        </body>
-      </html>
-    `);
-});
-
-app.get('/works', (request, response) => {
-  response.send(`
-    <!doctype html>
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <title>Works</title>
-        <link rel="stylesheet" href="/stylesheets/style.css" />
-      </head>
-      <body>
-        <h1>Work</h1>
-        <p>I've worked in logistic for more tha 3 years</p>
-      </body>
-    </html>
-  `);
-});
-
-app.listen(3000, () => console.log('My first app listening on port 3000! '));
